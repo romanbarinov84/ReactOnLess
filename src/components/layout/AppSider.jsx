6f1176df9efc9fea74a486d86dbfc15804c21fca
Roman <roman-meinl@ukr.net>
2025-02-27 18:15:50 -0200

    function parcentDifference AppSider.jsx

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,17 +1,84 @@
-import { Layout } from 'antd';
-
+import { Layout, Card ,Statistic,List,Typography,Spin} from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { useEffect, useState } from "react";
+import { fakeFetchCrypto,fetchAssets } from "./App";
 
 const siderStyle = {
-    textAlign: 'center',
-    lineHeight: '120px',
-    color: '#fff',
-    backgroundColor: '#1677ff',
-  };
-  
-export default function AppSider(){
-    return(
-        <Layout.Sider width="25%" style={siderStyle}>
-              Sider
-            </Layout.Sider>
-    )
+  padding: "1rem",
+};
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
+function percentDifference(a ,b){
+  return 100 * Math.abs((a - b) / ((a + b) / 2));
+}
+
+export default function AppSider() {
+  const [loading, setLoading] = useState(false)
+ const [crypto, setCrypto] = useState([])
+ const [assets, setAssets] = useState([])
+
+  useEffect(() => {
+   async function preLoad(){
+    setLoading(true)
+   const {result} = await fakeFetchCrypto()
+   const assets =  await fetchAssets()
+
+   setAssets(assets.map((asset) =>{
+    const coin = result.find((c) => c.id === asset.id)
+    return {
+      grow: asset.price < coin.price,
+      growPercent: percentDifference(asset.price, coin.price),
+      totalAmount:asset.amount * coin.price,
+      totalProfit:asset.amount * coin.price - asset.amount * asset.price,
+      ...asset
+    }
+   } ))
+   setCrypto(result)
+   setLoading(false)
+    }
+    preLoad()
+  },[])
+
+  if(loading){
+    return <Spin fullscreen />
+  }
+
+
+  return (
+    <Layout.Sider width="25%" style={siderStyle}>
+      <Card style={{marginBottom:"1rem"}}>
+        <Statistic  title="Active"
+          value={11.28}
+          precision={2}
+          valueStyle={{ color: '#3f8600' }}
+          prefix={<ArrowUpOutlined />}
+          suffix="%"/>
+           <List
+           size="small"
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item>
+          <Typography.Text mark>[ITEM]</Typography.Text> {item}
+        </List.Item>
+      )}
+    />
+      </Card>
+     
+      <Card>
+        <Statistic   title="Idle"
+          value={9.3}
+          precision={2}
+          valueStyle={{ color: '#cf1322' }}
+          prefix={<ArrowDownOutlined />}
+          suffix="%"/>
+      </Card>
+    </Layout.Sider>
+  );
 }
